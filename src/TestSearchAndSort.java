b1937c0dbb6d8464a519052e6dbae4b2f48a6457
adonayaraia02@gmail.com <adonayaraia02@gmail.com>
2025-09-19 17:35:42 -0400

    updated

--- src/TestSearchAndSort.java
+++ src/TestSearchAndSort.java
@@ -5,57 +5,73 @@ public class TestSearchAndSort {
     public static void main(String[] args) {
 
         // create array and populate and array with random numbers
-        int size = 1000;
-        int[] testData = new int[size];
-        Random rndm = new Random(); // use a Random object to generate data
-        for (int idx = 0; idx < size; idx++) {
-            testData[idx] = rndm.nextInt(1000); // Random integers between 0 & 1000
+        int[] size = { 100, 1000, 10000 };
+        // int[] testData = new int[size];
+        // Random rndm = new Random(); // use a Random object to generate data
+        for (int idx = 0; idx < size.length; idx++) {
+            int[] testData = createArray(size[idx]);
+            SortReturn sr = Sort.bubble(testData);
+            System.out.println(sr.toString());
+        }
+        for (int idx = 0; idx < size.length; idx++) {
+            int[] testData = createArray(size[idx]);
+            SortReturn sr = Sort.selection(testData);
+            System.out.println(sr.toString());
+        }
+        for (int idx = 0; idx < size.length; idx++) {
+            int[] testData = createArray(size[idx]);
+            SortReturn sr = Sort.Insertion(testData);
+            System.out.println(sr.toString());
+        }
+        for (int idx = 0; idx < size.length; idx++) {
+            // Declare a SearchReturn variable to use below
+            SearchReturn srchReturn;
+            int[] testData = createArray(size[idx]);
+            // Search for an element that is known to be in the 10% position
+            srchReturn = Search.linear(testData, testData[size[idx] / 10]);
+            System.out.println("10% " + srchReturn.toString()); // display the performance info
+
+            // Search for an element that is known to be in the 50% position
+            srchReturn = Search.linear(testData, testData[size[idx] / 2]);
+            System.out.println("50% " + srchReturn.toString()); // display the performance info
+
+            // Search for an element that is known to be in the 90% position
+            srchReturn = Search.linear(testData, testData[(int) (size[idx] * 0.9)]);
+            System.out.println("90% " + srchReturn.toString()); // display the performance info
+
+            // Search for an element that is known not to exist (
+            srchReturn = Search.linear(testData, 9999);
+            System.out.println("No element " + srchReturn.toString());
+        }
+        for (int idx = 0; idx < size.length; idx++) {
+            // Declare a SearchReturn variable to use below
+            SearchReturn srchReturn;
+            int[] testData = createArray(size[idx]);
+            // Search for an element that is known to be in the 10% position
+            srchReturn = Search.Binary(testData, testData[size[idx] / 10]);
+            System.out.println("10% " + srchReturn.toString()); // display the performance info
+
+            // Search for an element that is known to be in the 50% position
+            srchReturn = Search.Binary(testData, testData[size[idx] / 2]);
+            System.out.println("50% " + srchReturn.toString()); // display the performance info
+
+            // Search for an element that is known to be in the 90% position
+            srchReturn = Search.Binary(testData, testData[(int) (size[idx] * 0.9)]);
+            System.out.println("90% " + srchReturn.toString()); // display the performance info
+
+            // Search for an element that is known not to exist (
+            srchReturn = Search.Binary(testData, 9999);
+            System.out.println("No element " + srchReturn.toString());
         }
 
-        // Declare a SearchReturn variable to use below
-        SearchReturn srchReturn;
-
-        // Sort the array with bubble sort and print results
-        SortReturn sr = Sort.bubble(testData);
-        System.out.println(sr.toString()); // display the performance info
-        // selection
-        sr = Sort.selection(testData);
-        System.out.println(sr.toString());
         // insertion
-        sr = Sort.Insertion(testData);
-        System.out.println(sr.toString());
+
         // Searches -- pull an element out of the array (e.g. testData[position])
         // so that it's known to exist in the array
 
         // Search for an element that is known to be in the 10% position
-        srchReturn = Search.linear(testData, testData[size / 10]);
-        System.out.println(srchReturn.toString()); // display the performance info
-
-        // Search for an element that is known to be in the 50% position
-        srchReturn = Search.linear(testData, testData[size / 2]);
-        System.out.println(srchReturn.toString()); // display the performance info
-
-        // Search for an element that is known to be in the 90% position
-        srchReturn = Search.linear(testData, testData[(int) (size * 0.9)]);
-        System.out.println(srchReturn.toString()); // display the performance info
-
-        // Search for an element that is known not to exist (
-        srchReturn = Search.linear(testData, 9999);
-        System.out.println(srchReturn.toString()); // display the performance info
 
         // Binary search on a SORTED copy (don’t time the sorting here)
-
-        srchReturn = Search.Binary(testData, testData[size / 10]);
-        System.out.println(srchReturn.toString());
-
-        srchReturn = Search.Binary(testData, testData[size / 2]);
-        System.out.println(srchReturn.toString());
-
-        srchReturn = Search.Binary(testData, testData[(int) (size * 0.9)]);
-        System.out.println(srchReturn.toString());
-
-        srchReturn = Search.Binary(testData, 9999);
-        System.out.println(srchReturn.toString());
 
     }
 
@@ -66,4 +82,16 @@ public class TestSearchAndSort {
         }
         System.out.println();
     }
+
+    public static int[] createArray(int size) {
+        int[] testData = new int[size];
+        Random rdm = new Random();
+        for (int i = 0; i < size; i++) {
+            testData[i] = rdm.nextInt(10000);
+
+        }
+        return testData;
+
+    }
+
 }
